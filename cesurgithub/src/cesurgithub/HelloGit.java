d2113a1bfe2c7820d2249679bc013bb01275e337
idavid80 <idavid80@Lenovo>
2023-03-21 00:47:16 +0100

    push cesurgithub entorno

---
+++ cesurgithub/src/cesurgithub/HelloGit.java
@@ -0,0 +1,9 @@
+package cesurgithub;
+
+public class HelloGit {
+	public static void main(String[] args) {
+	String nombre = "David";
+	System.out.println("Hola github, soy " + nombre);
+	System.out.println("Hello github, I am " + nombre);
+	}
+}
