eef25411af7bcf9e7d2a07a9165245064b680274
idavid80 <idavid80@Lenovo>
2023-03-24 09:59:32 +0100

    Github con Eclipse

--- cesurgithub/src/cesurgithub/HelloGit.java
+++
@@ -1,9 +0,0 @@
-package cesurgithub;
-
-public class HelloGit {
-	public static void main(String[] args) {
-	String nombre = "David";
-	System.out.println("Hola github, soy " + nombre);
-	System.out.println("Hello github, I am " + nombre);
-	}
-}
