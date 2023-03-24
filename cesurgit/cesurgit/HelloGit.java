cd97a0b9e8b2e5a54ab2e5dea478da5c1ce07b08
idavid80 <idavid80@Lenovo>
2023-03-24 10:08:01 +0100

    subir archivo java

---
+++ cesurgit/cesurgit/HelloGit.java
@@ -0,0 +1,11 @@
+package cesurgit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hola github");
+        System.out.println("Hello github");
+	}
+
+}
