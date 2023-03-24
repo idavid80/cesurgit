b71103b06ddd47dfc30eaa8f86feb9628072ffc2
idavid80 <idavid80@Lenovo>
2023-03-24 10:11:34 +0100

    archivo Java

--- cesurgit/src/cesurgit/HelloGit.java
+++ cesurgit/src/cesurgit/HelloGit.java
@@ -2,6 +2,11 @@ package cesurgit;
 
 public class HelloGit {
 
+	public HelloGit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hola github");
